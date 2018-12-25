0799a5f48c84d8c9c777a6bb3b072458f6e65c03
Daniel Noyola <danielnv18@gmail.com>
2018-12-25 10:27:39 -0600

    Decode jwt.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,37 @@
 import cookieParser from "cookie-parser";
+import jwt from 'jsonwebtoken';
 import { createServer } from "./server";
+import { database } from './database';
 
 const server = createServer();
 server.use(cookieParser());
+
+// Decode the JWT so we can get the user Id on each request
+server.express.use((req, res, next) => {
+	const { token } = req.cookies
+	if (token) {
+		const userId = jwt.verify(token, process.env.APP_SECRET)
+		// put the userId onto the req for future requests to access
+		req.userId = userId;
+	}
+	next();
+});
+
+// Create a middleware that populates the user on each request
+server.express.use(async (req, res, next) => {
+	// if they aren't logged in, skip this
+	if (!req.userId) next();
+	const user = await database.query.user(
+    {
+      where: {
+        id: req.userId
+      }
+    },
+    '{ id, email, name }'
+	);
+	req.user = user;
+	next();
+});
 
 server.start({
 	cors: {
