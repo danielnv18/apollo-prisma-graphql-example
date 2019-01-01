f099b21747a5f68e78cc50b7f5858f5e49f83c80
Daniel Noyola <danielnv18@gmail.com>
2018-12-31 19:36:31 -0600

    fixed names part 2

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,7 +1,7 @@
 import { IResolvers, ITypes } from '../generated/resolvers'
 
-import { Query } from './_Query'
-import { Mutation } from './_Mutation'
+import { Query } from './Query'
+import { Mutation } from './Mutation'
 // import { User } from './User'
 
 export const resolvers: IResolvers<ITypes> = {
